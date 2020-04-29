4a60392e9b1ed81910fe44c13f2173459a8da64d
Malcolm Still <malcolm.still@gmail.com>
2020-04-29 23:48:05 +0100

    cmsg_firsthdr compiles
    So there were a couple of issues here:
    - including <sys/socket.h> and attempting to call the macros (e.g. c.CMSG_LEN) doesn't work. We get a compile error (from zig) about a void or not knowing the size
    of struct cmsghdr
    - even though it doesn't compile, zig nicely dumps out the full line that it's struggling with so I was able to dump these into recvmsg.zig and use a cmsghdr defined in the file to get stuff to work
    - cmsg_firsthdr was tougher due to an alignment issue. cmsghdr (with three 4-byte fields) has alignment 4 whereas msghdr has alignment 8. Looking at the definitions in Go for amd64 linux, the cmsg_len field is defined as a u64. Modifying the struct in zig to have that field as u64 means the alignment is 8 and the code compiles
    - I'm not sure how to go about making this portable. I have a feeling the cmsg functions would be portable (I believe the might be defined the same in every header that includes them) and they are there precisely because they are portable. The cmsghdr won't be portable, I guess so how do I generate that...or does it just have to be defined separately for each architecture / os?
    - Could we get some nice interface to recvmsg into the zig standard lib?

--- src/recvmsg.zig
+++ src/recvmsg.zig
@@ -51,5 +51,40 @@ pub fn recvMsg(fd: i32, buffer: []u8, fds: []i32) !usize {
 
     std.debug.warn("rc: {}, oobn: {}\n", .{ rc, oobn });
 
+    std.debug.warn("msghdr size: {}\n", .{ @sizeOf(linux.msghdr) });
+    std.debug.warn("msghdr alignment: {}\n", .{ @alignOf(linux.msghdr) });
+    std.debug.warn("cmsghdr size: {}\n", .{ @sizeOf(cmsghdr) });
+    std.debug.warn("cmsghdr alignment: {}\n", .{ @alignOf(cmsghdr) });
+    std.debug.warn("x: {}\n", .{ cmsg_len(@sizeOf(i32)) });
+    std.debug.warn("y: {}\n", .{ cmsg_firsthdr(@ptrCast(*linux.msghdr, &msg)) });
+
     return @intCast(usize, rc);
 }
+
+fn cmsg_space(size: comptime isize) c_int {
+    return @bitCast(c_int, @truncate(c_uint, ((((@bitCast(c_ulong, @as(c_long, (size))) +% @sizeOf(usize)) -% @bitCast(c_ulong, @as(c_long, @as(c_int, 1)))) & @bitCast(usize, ~(@sizeOf(usize) -% @bitCast(c_ulong, @as(c_long, @as(c_int, 1)))))) +% ((((@sizeOf(cmsghdr)) +% @sizeOf(usize)) -% @bitCast(c_ulong, @as(c_long, @as(c_int, 1)))) & @bitCast(usize, ~(@sizeOf(usize) -% @bitCast(c_ulong, @as(c_long, @as(c_int, 1)))))))));
+}
+
+fn cmsg_len(size: comptime isize) c_int {
+    return @bitCast(c_int, @truncate(c_uint, (((((@sizeOf(cmsghdr)) +% @sizeOf(usize)) -% @bitCast(c_ulong, @as(c_long, @as(c_int, 1)))) & @bitCast(usize, ~(@sizeOf(usize) -% @bitCast(c_ulong, @as(c_long, @as(c_int, 1)))))) +% @bitCast(c_ulong, @as(c_long, (size))))));                                        
+}
+
+fn cmsg_data(hdr: *cmsghdr) *i32 {
+    return @ptrCast([*c]c_int, @alignCast(@alignOf(c_int), &((@ptrCast(*cmsghdr, @alignCast(@alignOf(cmsghdr), hdr))))));
+}
+
+fn cmsg_firsthdr(msg: *linux.msghdr) ?*cmsghdr {
+    return @ptrCast(?*cmsghdr,
+        (if ((@ptrCast(*linux.msghdr, @alignCast(@alignOf(cmsghdr), msg))).msg_controllen >= @sizeOf(cmsghdr))
+            @ptrCast(*cmsghdr, @alignCast(@alignOf(cmsghdr), (@ptrCast(*linux.msghdr, @alignCast(@alignOf(linux.msghdr), msg))).msg_control))
+        else
+            @intToPtr(?*cmsghdr, @as(c_int, 0))));
+}
+
+pub const cmsghdr = extern struct {
+    // cmsg_len: linux.socklen_t does not work as this struct has align 4 (msghdr align 8 which upsets firsthdr)
+    // go has this value a u64 for amd64 linux
+    cmsg_len: u64, 
+    cmsg_level: c_int,
+    cmsg_type: c_int,
+};
