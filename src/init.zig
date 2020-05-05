acedad76733c4859e563e5de669db061b8a184d0
Malcolm Still <malcolm.still@gmail.com>
2020-05-05 15:05:48 +0100

    Initial Window work

--- src/init.zig
+++ src/init.zig
@@ -1,35 +1,40 @@
 const std = @import("std");
 const wl = @import("wl/protocols.zig");
 const Object = @import("wl/context.zig").Object;
+const win = @import("window.zig");
+const Window = @import("window.zig").Window;
 
 pub fn init() void {
     wl.WL_DISPLAY.sync = sync;
     wl.WL_DISPLAY.get_registry = get_registry;
     wl.WL_REGISTRY.bind = bind;
+    wl.WL_COMPOSITOR.create_surface = create_surface;
+    wl.XDG_WM_BASE.get_xdg_surface = get_xdg_surface;
 }
 
 fn sync(object: Object, new_id: u32) void {
     std.debug.warn("sync with id {}\n", .{new_id});
-    var callback = wl.new_wl_callback(object.context, new_id);
-    wl.wl_callback_send_done(callback, 0);
-    var x = object.context.unregister(callback);
-    wl.wl_display_send_delete_id(object, callback.id);
+    if(wl.new_wl_callback(object.context, new_id)) |callback| {
+        wl.wl_callback_send_done(callback.*, 0);
+        var x = object.context.unregister(callback.*);
+        wl.wl_display_send_delete_id(object, callback.id);
+    }
 }
 
 fn get_registry(object: Object, new_id: u32) void {
     std.debug.warn("get_registry with id {}\n", .{new_id});
-    var registry = wl.new_wl_registry(object.context, new_id);
-
-    wl.wl_registry_send_global(registry, 1, "wl_compositor\x00", 4);
-    wl.wl_registry_send_global(registry, 2, "wl_subcompositor\x00", 1);
-    wl.wl_registry_send_global(registry, 3, "wl_seat\x00", 4);
-    wl.wl_registry_send_global(registry, 4, "xdg_wm_base\x00", 1);
-    wl.wl_registry_send_global(registry, 5, "wl_output\x00", 2);
-    wl.wl_registry_send_global(registry, 6, "wl_data_device_manager\x00", 3);
-    wl.wl_registry_send_global(registry, 7, "wl_shell\x00", 1);
-    wl.wl_registry_send_global(registry, 8, "wl_shm\x00", 1);
-    wl.wl_registry_send_global(registry, 9, "zxdg_decoration_manager_v1\x00", 1);
-    wl.wl_registry_send_global(registry, 10, "zwp_linux_dmabuf_v1\x00", 3);
+    if (wl.new_wl_registry(object.context, new_id)) |registry| {
+        wl.wl_registry_send_global(registry.*, 1, "wl_compositor\x00", 4);
+        wl.wl_registry_send_global(registry.*, 2, "wl_subcompositor\x00", 1);
+        wl.wl_registry_send_global(registry.*, 3, "wl_seat\x00", 4);
+        wl.wl_registry_send_global(registry.*, 4, "xdg_wm_base\x00", 1);
+        wl.wl_registry_send_global(registry.*, 5, "wl_output\x00", 2);
+        wl.wl_registry_send_global(registry.*, 6, "wl_data_device_manager\x00", 3);
+        wl.wl_registry_send_global(registry.*, 7, "wl_shell\x00", 1);
+        wl.wl_registry_send_global(registry.*, 8, "wl_shm\x00", 1);
+        wl.wl_registry_send_global(registry.*, 9, "zxdg_decoration_manager_v1\x00", 1);
+        wl.wl_registry_send_global(registry.*, 10, "zwp_linux_dmabuf_v1\x00", 3);
+    }
 }
 
 fn bind(registry: Object, name: u32, name_string: []u8, version: u32, new_id: u32) void {
@@ -37,32 +42,24 @@ fn bind(registry: Object, name: u32, name_string: []u8, version: u32, new_id: u3
 
     switch (name) {
         1 => {
-            var compositor = wl.new_wl_compositor(registry.context, new_id);
-
-            if (registry.context.objects.get(compositor.id)) |c| {
-                c.value.version = version;
+            if(wl.new_wl_compositor(registry.context, new_id)) |compositor| {
+                compositor.version = version;
+                registry.context.client.compositor = compositor.id;
             }
-
-            registry.context.client.compositor = compositor.id;
         },
         2 => {
-            var subcompositor = wl.new_wl_subcompositor(registry.context, new_id);
-
-            if (registry.context.objects.get(subcompositor.id)) |s| {
-                s.value.version = version;
+            if(wl.new_wl_subcompositor(registry.context, new_id)) |subcompositor| {
+                subcompositor.version = version;
+                registry.context.client.subcompositor = subcompositor.id;
             }
-
-            registry.context.client.subcompositor = subcompositor.id;},
+        },
         3 => {
             if (registry.context.client.seat == null) {
-                var seat = wl.new_wl_seat(registry.context, new_id);
-
-                if (registry.context.objects.get(seat.id)) |s| {
-                    s.value.version = version;
-                    wl.wl_seat_send_capabilities(s.value, @enumToInt(wl.wl_seat_capability.pointer) | @enumToInt(wl.wl_seat_capability.keyboard));
+                if (wl.new_wl_seat(registry.context, new_id)) |seat| {
+                    seat.version = version;
+                    wl.wl_seat_send_capabilities(seat.*, @enumToInt(wl.wl_seat_capability.pointer) | @enumToInt(wl.wl_seat_capability.keyboard));
+                    registry.context.client.seat = seat.id;
                 }
-
-                registry.context.client.seat = seat.id;
             }
         },
         4 => {},
@@ -70,18 +67,33 @@ fn bind(registry: Object, name: u32, name_string: []u8, version: u32, new_id: u3
         6 => {},
         7 => {},
         8 => {
-            var shm = wl.new_wl_shm(registry.context, new_id);
+            if(wl.new_wl_shm(registry.context, new_id)) |shm| {
+                shm.version = version;
+                registry.context.client.shm = shm.id;
 
-            if (registry.context.objects.get(shm.id)) |s| {
-                s.value.version = version;
+                wl.wl_shm_send_format(shm.*, @enumToInt(wl.wl_shm_format.argb8888));
+                wl.wl_shm_send_format(shm.*, @enumToInt(wl.wl_shm_format.xrgb8888));
             }
-
-            wl.wl_shm_send_format(shm, @enumToInt(wl.wl_shm_format.argb8888));
-            wl.wl_shm_send_format(shm, @enumToInt(wl.wl_shm_format.xrgb8888));
-            registry.context.client.shm = shm.id;
         },
         9 => {},
         10 => {},
         else => {},
     }
+}
+
+fn create_surface(compositor: Object, id: u32) void {
+    var surface = wl.new_wl_surface(compositor.context, id);
+    var x = win.newWindow(compositor.context.client, id) ; // why can't I catch this?
+
+    return;
+}
+
+fn get_xdg_surface(base: Object, id: u32, surface: Object) void {
+    if (wl.new_xdg_surface(base.context, id)) |xdg_surface| {
+        var window = @intToPtr(*Window, surface.container);
+        window.xdg_surface = xdg_surface.id;
+        xdg_surface.container = @ptrToInt(window);
+    }
+
+    return;
 }
---
+++ src/window.zig
@@ -0,0 +1,64 @@
+const std = @import("std");
+const Client = @import("client.zig").Client;
+
+const MAX_WINDOWS = 512;
+var WINDOWS: [MAX_WINDOWS]Window = undefined;
+
+pub const Window = struct {
+    index: usize = 0,
+    in_use: bool = false,
+    state: [2]BufferedState = undefined,
+    buffer: u1 = 0,
+    surface: u32,
+    xdg_surface: ?u32,
+    client: *Client,
+    geometry: [24]f32 = undefined,
+
+    const Self = @This();
+
+    // flip double-buffered state
+    pub fn flip(self: *Self) void {
+        self.buffer +%= 1;
+    }
+
+    pub fn pending(self: *Self) *BufferedState {
+        return &self.state[self.buffer +% 1];
+    }
+
+    pub fn deinit(self: *Self) void {
+        WINDOWS[self.index].in_use = false;
+    }
+};
+
+pub fn newWindow(client: *Client, surface: u32) !*Window {
+    var i: usize = 0;
+    while (i < MAX_WINDOWS) {
+        if (WINDOWS[i].in_use == false) {
+            WINDOWS[i].index = i;
+            WINDOWS[i].in_use = true;
+            WINDOWS[i].client = client;
+            WINDOWS[i].surface = surface;
+            if (client.context.objects.get(surface)) |s| {
+                s.value.container = @ptrToInt(&WINDOWS[i]);
+            }
+
+            return &WINDOWS[i];
+        } else {
+            i = i + 1;
+            continue;
+        }
+    }
+
+    return WindowsError.WindowsExhausted;
+}
+
+const BufferedState = struct {
+    x: i32 = 0,
+    y: i32 = 0,
+    width: u32 = 0,
+    height: u32 = 0,
+};
+
+const WindowsError = error {
+    WindowsExhausted,
+};
