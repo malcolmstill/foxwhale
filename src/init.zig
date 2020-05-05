59eb1925e41f2d58d483a2670bf2858d4e300944
Malcolm Still <malcolm.still@gmail.com>
2020-05-05 19:46:56 +0100

    init: anyerror!void and try

--- src/init.zig
+++ src/init.zig
@@ -3,6 +3,7 @@ const wl = @import("wl/protocols.zig");
 const Object = @import("wl/context.zig").Object;
 const win = @import("window.zig");
 const Window = @import("window.zig").Window;
+const shm_impl = @import("shm.zig");
 
 pub fn init() void {
     wl.WL_DISPLAY.sync = sync;
@@ -11,37 +12,40 @@ pub fn init() void {
     wl.WL_COMPOSITOR.create_surface = create_surface;
     wl.XDG_WM_BASE.get_xdg_surface = get_xdg_surface;
     wl.XDG_SURFACE.get_toplevel = get_toplevel;
+    wl.XDG_SURFACE.ack_configure = ack_configure;
     wl.XDG_TOPLEVEL.set_title = set_title;
     wl.WL_SURFACE.commit = commit;
     wl.WL_SURFACE.damage = damage;
+
+    shm_impl.init();
 }
 
-fn sync(object: Object, new_id: u32) void {
+fn sync(object: Object, new_id: u32) anyerror!void {
     std.debug.warn("sync with id {}\n", .{new_id});
     if(wl.new_wl_callback(object.context, new_id)) |callback| {
-        wl.wl_callback_send_done(callback.*, 120);
+        try wl.wl_callback_send_done(callback.*, 120);
         var x = object.context.unregister(callback.*);
-        wl.wl_display_send_delete_id(object, callback.id);
+        try wl.wl_display_send_delete_id(object, callback.id);
     }
 }
 
-fn get_registry(object: Object, new_id: u32) void {
+fn get_registry(object: Object, new_id: u32) anyerror!void {
     std.debug.warn("get_registry with id {}\n", .{new_id});
     if (wl.new_wl_registry(object.context, new_id)) |registry| {
-        wl.wl_registry_send_global(registry.*, 1, "wl_compositor\x00", 4);
-        wl.wl_registry_send_global(registry.*, 2, "wl_subcompositor\x00", 1);
-        wl.wl_registry_send_global(registry.*, 3, "wl_seat\x00", 4);
-        wl.wl_registry_send_global(registry.*, 4, "xdg_wm_base\x00", 1);
-        wl.wl_registry_send_global(registry.*, 5, "wl_output\x00", 2);
-        wl.wl_registry_send_global(registry.*, 6, "wl_data_device_manager\x00", 3);
-        wl.wl_registry_send_global(registry.*, 7, "wl_shell\x00", 1);
-        wl.wl_registry_send_global(registry.*, 8, "wl_shm\x00", 1);
-        wl.wl_registry_send_global(registry.*, 9, "zxdg_decoration_manager_v1\x00", 1);
-        wl.wl_registry_send_global(registry.*, 10, "zwp_linux_dmabuf_v1\x00", 3);
+        try wl.wl_registry_send_global(registry.*, 1, "wl_compositor\x00", 4);
+        try wl.wl_registry_send_global(registry.*, 2, "wl_subcompositor\x00", 1);
+        try wl.wl_registry_send_global(registry.*, 3, "wl_seat\x00", 4);
+        try wl.wl_registry_send_global(registry.*, 4, "xdg_wm_base\x00", 1);
+        try wl.wl_registry_send_global(registry.*, 5, "wl_output\x00", 2);
+        try wl.wl_registry_send_global(registry.*, 6, "wl_data_device_manager\x00", 3);
+        try wl.wl_registry_send_global(registry.*, 7, "wl_shell\x00", 1);
+        try wl.wl_registry_send_global(registry.*, 8, "wl_shm\x00", 1);
+        try wl.wl_registry_send_global(registry.*, 9, "zxdg_decoration_manager_v1\x00", 1);
+        try wl.wl_registry_send_global(registry.*, 10, "zwp_linux_dmabuf_v1\x00", 3);
     }
 }
 
-fn bind(registry: Object, name: u32, name_string: []u8, version: u32, new_id: u32) void {
+fn bind(registry: Object, name: u32, name_string: []u8, version: u32, new_id: u32) anyerror!void {
     std.debug.warn("bind for {} ({}) with id {} at version {}\n", .{name_string, name, new_id, version});
 
     switch (name) {
@@ -61,7 +65,7 @@ fn bind(registry: Object, name: u32, name_string: []u8, version: u32, new_id: u3
             if (registry.context.client.seat == null) {
                 if (wl.new_wl_seat(registry.context, new_id)) |seat| {
                     seat.version = version;
-                    wl.wl_seat_send_capabilities(seat.*, @enumToInt(wl.wl_seat_capability.pointer) | @enumToInt(wl.wl_seat_capability.keyboard));
+                    try wl.wl_seat_send_capabilities(seat.*, @enumToInt(wl.wl_seat_capability.pointer) | @enumToInt(wl.wl_seat_capability.keyboard));
                     registry.context.client.seat = seat.id;
                 }
             }
@@ -80,8 +84,8 @@ fn bind(registry: Object, name: u32, name_string: []u8, version: u32, new_id: u3
                 shm.version = version;
                 registry.context.client.shm = shm.id;
 
-                wl.wl_shm_send_format(shm.*, @enumToInt(wl.wl_shm_format.argb8888));
-                wl.wl_shm_send_format(shm.*, @enumToInt(wl.wl_shm_format.xrgb8888));
+                try wl.wl_shm_send_format(shm.*, @enumToInt(wl.wl_shm_format.argb8888));
+                try wl.wl_shm_send_format(shm.*, @enumToInt(wl.wl_shm_format.xrgb8888));
             }
         },
         9 => {},
@@ -90,14 +94,14 @@ fn bind(registry: Object, name: u32, name_string: []u8, version: u32, new_id: u3
     }
 }
 
-fn create_surface(compositor: Object, id: u32) void {
+fn create_surface(compositor: Object, id: u32) anyerror!void {
     std.debug.warn("create_surface: {}\n", .{id});
     if (wl.new_wl_surface(compositor.context, id)) |surface| {
         var x = win.newWindow(compositor.context.client, id);
     }
 }
 
-fn get_xdg_surface(base: Object, id: u32, surface: Object) void {
+fn get_xdg_surface(base: Object, id: u32, surface: Object) anyerror!void {
     std.debug.warn("get_xdg_surface: {}\n", .{id});
     if (wl.new_xdg_surface(base.context, id)) |xdg_surface| {
         var window = @intToPtr(*Window, surface.container);
@@ -106,7 +110,7 @@ fn get_xdg_surface(base: Object, id: u32, surface: Object) void {
     }
 }
 
-fn get_toplevel(xdg_surface: Object, id: u32) void {
+fn get_toplevel(xdg_surface: Object, id: u32) anyerror!void {
     std.debug.warn("get_toplevel: {}\n", .{id});
     if (wl.new_xdg_toplevel(xdg_surface.context, id)) |xdg_toplevel| {
         var window = @intToPtr(*Window, xdg_surface.container);
@@ -115,22 +119,26 @@ fn get_toplevel(xdg_surface: Object, id: u32) void {
 
         var array = [_]u32{};
         var serial = window.client.nextSerial();
-        wl.xdg_toplevel_send_configure(xdg_toplevel.*, 0, 0, array[0..array.len]);
-        wl.xdg_surface_send_configure(xdg_surface, serial);
+        try wl.xdg_toplevel_send_configure(xdg_toplevel.*, 0, 0, array[0..array.len]);
+        try wl.xdg_surface_send_configure(xdg_surface, serial);
     }
 }
 
-fn set_title(xdg_toplevel: Object, title: []u8) void {
+fn set_title(xdg_toplevel: Object, title: []u8) anyerror!void {
     var window = @intToPtr(*Window, xdg_toplevel.container);
     var len = std.math.min(window.title.len, title.len);
     std.mem.copy(u8, window.title[0..len], title[0..len]);
     std.debug.warn("window: {}\n", .{window.title});
 }
 
-fn commit(surface: Object) void {
+fn commit(surface: Object) anyerror!void {
     std.debug.warn("surface {} committed\n", .{surface.id});
 }
 
-fn damage(object: Object, x: i32, y: i32, width: i32, height: i32) void {
+fn damage(object: Object, x: i32, y: i32, width: i32, height: i32) anyerror!void {
     std.debug.warn("damage does nothing\n", .{});
+}
+
+fn ack_configure(object: Object, serial: u32) anyerror!void {
+    std.debug.warn("ack_configure empty implementation\n", .{});
 }
