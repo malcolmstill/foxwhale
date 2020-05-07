97fa6861c70c1d2237fc96ecdd6d66304afd9be3
Malcolm Still <malcolm.still@gmail.com>
2020-05-07 22:54:21 +0100

    render: orthographicProjection

--- src/render.zig
+++ src/render.zig
@@ -5,8 +5,17 @@ const c = @cImport({
     @cInclude("GLES2/gl2.h");
 });
 
+var ortho: [16]f32 = undefined;
+
 pub fn render() void {
-    // std.debug.warn("vertex: {}\n", .{vertex_shader});
+    c.glClearColor(0.0, 0.6, 0.0, 0.0);
+    c.glClear(c.GL_COLOR_BUFFER_BIT | c.GL_DEPTH_BUFFER_BIT);
+    c.glUseProgram(PROGRAM);
+
+    c.glEnable(c.GL_BLEND);
+    c.glBlendFunc(c.GL_SRC_ALPHA, c.GL_ONE_MINUS_SRC_ALPHA);
+
+    orthographicProjection(&ortho, 0.0, 1600.0, 0.0, 1200.0, -1.0, 1.0);
 }
 
 var PROGRAM: c_uint = undefined;
@@ -45,4 +54,11 @@ fn compileShader(source: []const u8, shader_type: c_uint) !c_uint {
     }
 
     return shader;
+}
+
+fn orthographicProjection(m: *[16]f32, left: f32, right: f32, top: f32, bottom: f32, near: f32, far: f32) void {
+    m[0] = 2.0/(right-left);  m[1] = 0.0;               m[2] = 0.0;              m[3] = -((right + left)/(right-left));
+    m[4] = 0.0;               m[5] = 2.0/(top-bottom);  m[6] = 0.0;              m[7] = -((top + bottom)/(top-bottom));
+    m[8] = 0.0;               m[9] = 0.0;               m[10] = -2.0/(far-near); m[11] = -((far + near)/(far-near));
+    m[12] = 0.0;              m[13] = 0.0;              m[14] = 0.0;             m[15] = 1.0;
 }
