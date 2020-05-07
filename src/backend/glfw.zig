a946d5c2507efe80062b03083afe4b59e8943eb9
Malcolm Still <malcolm.still@gmail.com>
2020-05-07 06:05:57 +0100

    glfw: some callbacks to grab mouse / escape ungrabs mouse / escape again exits

--- src/backend/glfw.zig
+++ src/backend/glfw.zig
@@ -37,10 +37,38 @@ pub fn init() !GLFWBackend {
     var window = c.glfwCreateWindow(640, 480, "zig-wayland", null, null) orelse return error.GLFWWindowCreationFailed;
 
     c.glfwMakeContextCurrent(window);
+    _ = c.glfwSetKeyCallback(window, keyCallback);
+    _ = c.glfwSetWindowFocusCallback(window, windowFocusCallback);
+    _ = c.glfwSetMouseButtonCallback(window, mouseButtonCallback);
+
     c.glfwSwapInterval(1);
-    c.glClearColor(1.0, 0.0, 0.0, 0.0);
+    c.glClearColor(0.3, 0.3, 0.35, 0.0);
 
     return GLFWBackend {
         .window = window,
     };
+}
+
+fn keyCallback(window: ?*c.GLFWwindow, key: c_int, scancode: c_int, action: c_int, mods: c_int) callconv(.C) void {
+    if (key == c.GLFW_KEY_ESCAPE and action == c.GLFW_PRESS) {
+        if (c.glfwGetInputMode(window, c.GLFW_CURSOR) == c.GLFW_CURSOR_DISABLED) {
+            c.glfwSetInputMode(window, c.GLFW_CURSOR, c.GLFW_CURSOR_NORMAL);
+        } else {
+            c.glfwSetWindowShouldClose(window, c.GLFW_TRUE);
+        }
+    }
+}
+
+fn windowFocusCallback(window: ?*c.GLFWwindow, focussed: c_int) callconv(.C) void {
+    if (focussed == c.GLFW_TRUE) {
+        c.glfwSetInputMode(window, c.GLFW_CURSOR, c.GLFW_CURSOR_DISABLED);
+    } else {
+        c.glfwSetInputMode(window, c.GLFW_CURSOR, c.GLFW_CURSOR_NORMAL);
+    }
+}
+
+fn  mouseButtonCallback(window: ?*c.GLFWwindow, button: c_int, action: c_int, mods: c_int) callconv(.C) void {
+    if (action == c.GLFW_PRESS) {
+        c.glfwSetInputMode(window, c.GLFW_CURSOR, c.GLFW_CURSOR_DISABLED);
+    }
 }
