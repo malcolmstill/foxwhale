0b2e2eb297cd8cce1e1dade395dbfb28b3e44f28
Malcolm Still <malcolm.still@gmail.com>
2020-05-07 04:51:52 +0100

    init: comment out currently unsupported interfaces

--- src/init.zig
+++ src/init.zig
@@ -46,11 +46,11 @@ fn get_registry(context: *Context, wl_display: Object, new_id: u32) anyerror!voi
     try prot.wl_registry_send_global(wl_registry, 3, "wl_seat\x00", 4);
     try prot.wl_registry_send_global(wl_registry, 4, "xdg_wm_base\x00", 1);
     try prot.wl_registry_send_global(wl_registry, 5, "wl_output\x00", 2);
-    try prot.wl_registry_send_global(wl_registry, 6, "wl_data_device_manager\x00", 3);
+    // try prot.wl_registry_send_global(wl_registry, 6, "wl_data_device_manager\x00", 3);
     try prot.wl_registry_send_global(wl_registry, 7, "wl_shell\x00", 1);
     try prot.wl_registry_send_global(wl_registry, 8, "wl_shm\x00", 1);
-    try prot.wl_registry_send_global(wl_registry, 9, "zxdg_decoration_manager_v1\x00", 1);
-    try prot.wl_registry_send_global(wl_registry, 10, "zwp_linux_dmabuf_v1\x00", 3);
+    // try prot.wl_registry_send_global(wl_registry, 9, "zxdg_decoration_manager_v1\x00", 1);
+    // try prot.wl_registry_send_global(wl_registry, 10, "zwp_linux_dmabuf_v1\x00", 3);
 
     try context.register(wl_registry);
 }
