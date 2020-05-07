a6a60f88ed7674ff5222d439d77d0b91b0f854f1
Malcolm Still <malcolm.still@gmail.com>
2020-05-07 05:14:47 +0100

    wl_display: create file and move functions from implementations.zig

--- src/implementations.zig
+++ src/implementations.zig
@@ -4,6 +4,7 @@ const Context = @import("wl/context.zig").Context;
 const Object = @import("wl/context.zig").Object;
 
 const wl_compositor_impl = @import("wl_compositor.zig");
+const wl_display_impl = @import("wl_display.zig");
 const wl_seat_impl = @import("wl_seat.zig");
 const wl_shm_pool_impl = @import("wl_shm_pool.zig");
 const wl_shm_buffer_impl = @import("wl_shm_buffer.zig");
@@ -13,11 +14,10 @@ const xdg_surface_impl = @import("xdg_surface.zig");
 const xdg_toplevel_impl = @import("xdg_toplevel.zig");
 
 pub fn init() void {
-    prot.WL_DISPLAY.sync = sync;
-    prot.WL_DISPLAY.get_registry = get_registry;
     prot.WL_REGISTRY.bind = bind;
 
     wl_compositor_impl.init();
+    wl_display_impl.init();
     wl_seat_impl.init();
     wl_shm_pool_impl.init();
     wl_shm_buffer_impl.init();
@@ -26,33 +26,6 @@ pub fn init() void {
     xdg_base_impl.init();
     xdg_surface_impl.init();
     xdg_toplevel_impl.init();
-}
-
-fn sync(context: *Context, wl_display: Object, new_id: u32) anyerror!void {
-    std.debug.warn("sync with id {}\n", .{new_id});
-
-    var wl_callback = prot.new_wl_callback(new_id, context, 0);
-    try prot.wl_callback_send_done(wl_callback, @intCast(u32, std.time.timestamp()));
-    try prot.wl_display_send_delete_id(wl_display, wl_callback.id);
-}
-
-fn get_registry(context: *Context, wl_display: Object, new_id: u32) anyerror!void {
-    std.debug.warn("get_registry with id {}\n", .{new_id});
-
-    var wl_registry = prot.new_wl_registry(new_id, context, 0);
-
-    try prot.wl_registry_send_global(wl_registry, 1, "wl_compositor\x00", 4);
-    try prot.wl_registry_send_global(wl_registry, 2, "wl_subcompositor\x00", 1);
-    try prot.wl_registry_send_global(wl_registry, 3, "wl_seat\x00", 4);
-    try prot.wl_registry_send_global(wl_registry, 4, "xdg_wm_base\x00", 1);
-    try prot.wl_registry_send_global(wl_registry, 5, "wl_output\x00", 2);
-    // try prot.wl_registry_send_global(wl_registry, 6, "wl_data_device_manager\x00", 3);
-    try prot.wl_registry_send_global(wl_registry, 7, "wl_shell\x00", 1);
-    try prot.wl_registry_send_global(wl_registry, 8, "wl_shm\x00", 1);
-    // try prot.wl_registry_send_global(wl_registry, 9, "zxdg_decoration_manager_v1\x00", 1);
-    // try prot.wl_registry_send_global(wl_registry, 10, "zwp_linux_dmabuf_v1\x00", 3);
-
-    try context.register(wl_registry);
 }
 
 fn bind(context: *Context, wl_registry: Object, name: u32, name_string: []u8, version: u32, new_id: u32) anyerror!void {
---
+++ src/wl_display.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+const prot = @import("wl/protocols.zig");
+const Context = @import("wl/context.zig").Context;
+const Object = @import("wl/context.zig").Object;
+
+fn sync(context: *Context, wl_display: Object, new_id: u32) anyerror!void {
+    std.debug.warn("sync with id {}\n", .{new_id});
+
+    var wl_callback = prot.new_wl_callback(new_id, context, 0);
+    try prot.wl_callback_send_done(wl_callback, @intCast(u32, std.time.timestamp()));
+    try prot.wl_display_send_delete_id(wl_display, wl_callback.id);
+}
+
+fn get_registry(context: *Context, wl_display: Object, new_id: u32) anyerror!void {
+    std.debug.warn("get_registry with id {}\n", .{new_id});
+
+    var wl_registry = prot.new_wl_registry(new_id, context, 0);
+
+    try prot.wl_registry_send_global(wl_registry, 1, "wl_compositor\x00", 4);
+    try prot.wl_registry_send_global(wl_registry, 2, "wl_subcompositor\x00", 1);
+    try prot.wl_registry_send_global(wl_registry, 3, "wl_seat\x00", 4);
+    try prot.wl_registry_send_global(wl_registry, 4, "xdg_wm_base\x00", 1);
+    try prot.wl_registry_send_global(wl_registry, 5, "wl_output\x00", 2);
+    // try prot.wl_registry_send_global(wl_registry, 6, "wl_data_device_manager\x00", 3);
+    try prot.wl_registry_send_global(wl_registry, 7, "wl_shell\x00", 1);
+    try prot.wl_registry_send_global(wl_registry, 8, "wl_shm\x00", 1);
+    // try prot.wl_registry_send_global(wl_registry, 9, "zxdg_decoration_manager_v1\x00", 1);
+    // try prot.wl_registry_send_global(wl_registry, 10, "zwp_linux_dmabuf_v1\x00", 3);
+
+    try context.register(wl_registry);
+}
+
+pub fn init() void {
+    prot.WL_DISPLAY.sync = sync;
+    prot.WL_DISPLAY.get_registry = get_registry;
+}
