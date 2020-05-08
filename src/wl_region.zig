7177af13fc43996ae0cf01b41b6489906a681946
Malcolm Still <malcolm.still@gmail.com>
2020-05-08 21:32:32 +0100

    wl_region: implement add and subtract

--- src/wl_region.zig
+++ src/wl_region.zig
@@ -3,13 +3,35 @@ const prot = @import("wl/protocols.zig");
 const Context = @import("wl/context.zig").Context;
 const Object = @import("wl/context.zig").Object;
 const Region = @import("region.zig").Region;
+const RegionOp = @import("region.zig").RegionOp;
+const Rectangle = @import("region.zig").Rectangle;
 
 fn add(context: *Context, wl_region: Object, x: i32, y: i32, width: i32, height: i32) anyerror!void {
-    // IMPLEMENT
+    var region = @intToPtr(*Region, wl_region.container);
+
+    var rect = Rectangle {
+        .op = RegionOp.Add,
+        .x = x,
+        .y = y,
+        .width = width,
+        .height = height,
+    };
+
+    try region.pending().rectangles.writeItem(rect);
 }
 
 fn subtract(context: *Context, wl_region: Object, x: i32, y: i32, width: i32, height: i32) anyerror!void {
-    // IMPLEMENT
+    var region = @intToPtr(*Region, wl_region.container);
+
+    var rect = Rectangle {
+        .op = RegionOp.Subtract,
+        .x = x,
+        .y = y,
+        .width = width,
+        .height = height,
+    };
+
+    try region.pending().rectangles.writeItem(rect);
 }
 
 fn destroy(context: *Context, wl_region: Object) anyerror!void {
