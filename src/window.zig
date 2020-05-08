90210c26ac9f5089447f122e44e940be841a9cc7
Malcolm Still <malcolm.still@gmail.com>
2020-05-08 14:44:43 +0100

    window: properly init / deinit texture
    - This fixes a new client potentially using an old (and wrong) texture
      until it commits its first buffer

--- src/window.zig
+++ src/window.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const renderer = @import("renderer.zig");
 const Client = @import("client.zig").Client;
 const LinearFifo = std.fifo.LinearFifo;
 const LinearFifoBufferType = std.fifo.LinearFifoBufferType;
@@ -9,19 +10,20 @@ pub var WINDOWS: [MAX_WINDOWS]Window = undefined;
 pub const Window = struct {
     index: usize = 0,
     in_use: bool = false,
+    client: *Client,
 
     texture: ?u32,
     width: i32,
     height: i32,
 
-    state: [2]BufferedState = undefined,
-    stateIndex: u1 = 0,
     wl_surface_id: u32,
     wl_buffer_id: ?u32,
     xdg_surface_id: ?u32,
     xdg_toplevel_id: ?u32,
-    client: *Client,
-    geometry: [24]f32 = undefined,
+
+    state: [2]BufferedState = undefined,
+    stateIndex: u1 = 0,
+
     title: [128]u8 = undefined,
     callbacks: LinearFifo(u32, LinearFifoBufferType{ .Static = 32 }),
 
@@ -36,12 +38,23 @@ pub const Window = struct {
         return &self.state[self.stateIndex +% 1];
     }
 
-    pub fn deinit(self: *Self) void {
+    pub fn deinit(self: *Self) !void {
         std.debug.warn("release window\n", .{});
         self.in_use = false;
         self.wl_buffer_id = null;
         self.xdg_surface_id = null;
         self.xdg_toplevel_id = null;
+
+        self.width = 0;
+        self.height = 0;
+
+        if (self.texture) |texture| {
+            self.texture = null;
+            // Note that while this can fail, we're doing
+            // the bits that can fail after deinitialising
+            // enough so that this window could be reused
+            try renderer.releaseTexture(texture);
+        }
     }
 };
 
@@ -53,10 +66,15 @@ pub fn newWindow(client: *Client, wl_surface_id: u32) !*Window {
             window.index = i;
             window.in_use = true;
             window.client = client;
+
             window.wl_surface_id = wl_surface_id;
             window.wl_buffer_id = null;
             window.xdg_surface_id = null;
             window.xdg_toplevel_id = null;
+
+            window.texture = null;
+            window.width = 0;
+            window.height = 0;
 
             return window;
         } else {
@@ -79,12 +97,12 @@ const WindowsError = error {
     WindowsExhausted,
 };
 
-pub fn releaseWindows(client: *Client) void {
+pub fn releaseWindows(client: *Client) !void {
     var i: usize = 0;
     while (i < MAX_WINDOWS) {
         var window: *Window = &WINDOWS[i];
         if (window.in_use and window.client == client) {
-            window.deinit();
+            try window.deinit();
         }
         i = i + 1;
     }
