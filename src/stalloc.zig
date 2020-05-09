91c446c3e06108bcd3c41345eea88eb45bf47cff
Malcolm Still <malcolm.still@gmail.com>
2020-05-09 18:59:19 +0100

    stallic: abstract out the common static allocation code
    - Has tests
    - I'm only going to use this initially in Region
    - releaseBelongingTo returns void but value.deinit() may error so
      in debug we return the error otherwise we just log the error...
      the underlying region will still be freed for future use

---
+++ src/stalloc.zig
@@ -0,0 +1,124 @@
+const std = @import("std");
+
+pub fn Stalloc(comptime B: type, comptime T: type, comptime S: usize) type {
+    return struct {
+        data: [S]Entry,
+
+        const Self = @This();
+
+        const Entry = struct {
+            in_use: bool,
+            index: usize,
+            belongs_to: *B,
+            value: T,
+        };
+
+        pub fn new(self: *Self, belongs_to: *B) !*T {
+            var i: usize = 0;
+            while (i < S) {
+                var e: *Entry = &self.data[i];
+                if (e.in_use == false) {
+                    e.index = i;
+                    e.in_use = true;
+                    e.belongs_to = belongs_to;
+
+                    return &e.value;
+                } else {
+                    i = i + 1;
+                    continue;
+                }
+            }
+
+            return error.StallocExhausted;
+        }
+
+        pub fn deinit(self: *Self, t: *T) void {
+            var entry: *Entry = @fieldParentPtr(Entry, "value", t);
+            entry.in_use = false;
+        }
+
+        pub fn releaseBelongingTo(self: *Self, b: *B) !void {
+            var i: usize = 0;
+            while (i < S) {
+                var entry: *Entry = &self.data[i];
+                if (entry.in_use and entry.belongs_to == b) {
+                    entry.in_use = false;
+                    entry.value.deinit() catch |err| {
+                        if (std.builtin.mode == std.builtin.Mode.Debug) {
+                            return err;
+                        } else {
+                            std.debug.warn("warning: error in releaseBelongingTo\n", .{});
+                        }
+                    };
+                }
+                i = i + 1;
+            }
+        }
+
+        pub fn freeCount(self: *Self) usize {
+            var i: usize = 0;
+            var count: usize = 0;
+            while (i < S) {
+                var entry: *Entry = &self.data[i];
+                if (!entry.in_use) {
+                    count += 1;
+                }
+                i += 1;
+            }
+
+            return count;
+        }
+    };
+}
+
+const TestClient = struct {
+    i: u32,
+};
+
+const Payload = struct {
+    data: i32,
+
+    pub fn deinit(self: *Payload) !void {
+        self.data = 0;
+    }
+};
+
+var memory: Stalloc(TestClient, Payload, 4) = undefined;
+
+test "stalloc test" {
+    var test_client_1 = TestClient {
+        .i = 1,
+    };
+
+    var test_client_2 = TestClient {
+        .i = 2,
+    };    
+
+    std.debug.assert(memory.freeCount() == 4);
+
+    var x1 = try memory.new(&test_client_1);
+    std.debug.assert(memory.freeCount() == 3);
+
+    var x2 = try memory.new(&test_client_1);
+    std.debug.assert(memory.freeCount() == 2);
+
+    var x3 = try memory.new(&test_client_2);
+    std.debug.assert(memory.freeCount() == 1);
+
+    var x4 = try memory.new(&test_client_2);
+    std.debug.assert(memory.freeCount() == 0);
+
+    memory.deinit(x1);
+    std.debug.assert(memory.freeCount() == 1);
+
+    x1 = try memory.new(&test_client_1);
+    std.debug.assert(memory.freeCount() == 0);
+
+    var a: anyerror!*Payload = memory.new(&test_client_1);
+    if (a) |_| {} else |err| {
+        std.debug.assert(err == error.StallocExhausted);
+    }
+
+    try memory.releaseBelongingTo(&test_client_1);
+    std.debug.assert(memory.freeCount() == 2);
+}
