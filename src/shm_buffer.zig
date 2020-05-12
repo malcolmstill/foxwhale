4cccab33caff00db06b15816adc5612ceae6d89d
Malcolm Still <malcolm.still@gmail.com>
2020-05-12 23:07:22 +0100

    shm_buffer.zig: remove comment

--- src/shm_buffer.zig
+++ src/shm_buffer.zig
@@ -52,7 +52,6 @@ pub const ShmBuffer = struct {
     const Self = @This();
 
     pub fn deinit(self: *Self) void {
-        std.debug.warn("deinit buffer\n", .{});
         self.in_use = false;
     }
 
