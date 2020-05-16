ccdb600f6005ea533d4b67378dd8e982368189dd
Malcolm Still <malcolm.still@gmail.com>
2020-05-16 16:36:00 +0100

    wl_subsurface.zig: place_above / place_below implementations

--- src/wl_subsurface.zig
+++ src/wl_subsurface.zig
@@ -14,12 +14,62 @@ fn set_position(context: *Context, wl_subsurface: Object, x: i32, y: i32) anyerr
     window.pending().y = y;
 }
 
-fn place_above(context: *Context, object: Object, sibling: Object) anyerror!void {
-    return error.DebugFunctionNotImplemented;
+fn place_above(context: *Context, wl_subsurface: Object, wl_surface_sibling: Object) anyerror!void {
+    var window = @intToPtr(*Window, wl_subsurface.container);
+    var sibling = @intToPtr(*Window, wl_surface_sibling.container);
+
+    // 1. Detach window
+    if (window.current().prev) |prev| {
+        prev.pending().next = window.current().next;
+    }
+
+    if (window.current().next) |next| {
+        next.pending().prev = window.current().prev;
+    }
+
+    // 2. window.next may end up being null
+    window.pending().next = null;
+
+    // 3. window.prev will definitely be sibling
+    window.pending().prev = sibling;
+
+    // 4. if sibling has next, then next.prev is window and window.next is sibling.next
+    if (sibling.current().next) |next| {
+        next.pending().prev = window;
+        window.pending().next = sibling.current().next;
+    }
+
+    // 5. sibling.next becomes window
+    sibling.pending().next = window;
 }
 
-fn place_below(context: *Context, object: Object, sibling: Object) anyerror!void {
-    return error.DebugFunctionNotImplemented;
+fn place_below(context: *Context, wl_subsurface: Object, wl_surface_sibling: Object) anyerror!void {
+    var window = @intToPtr(*Window, wl_subsurface.container);
+    var sibling = @intToPtr(*Window, wl_surface_sibling.container);
+
+    // 1. Detach window
+    if (window.current().prev) |prev| {
+        prev.pending().next = window.current().next;
+    }
+
+    if (window.current().next) |next| {
+        next.pending().prev = window.current().prev;
+    }
+
+    // 2. window.prev may end up being null
+    window.pending().prev = null;
+
+    // 3. window.next will definitely be sibling
+    window.pending().next = sibling;
+
+    // 4. if sibling has prev, then prev.next is window and window.prev is sibling.prev
+    if (sibling.current().prev) |prev| {
+        prev.pending().next = window;
+        window.pending().prev = sibling.current().prev;
+    }
+
+    // 5. sibling.prev becomes window
+    sibling.pending().prev = window;
 }
 
 fn set_sync(context: *Context, wl_subsurface: Object) anyerror!void { 
