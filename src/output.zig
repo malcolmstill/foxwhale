7000319ced627cec585620686ed2b9590e183bda
Malcolm Still <malcolm.still@gmail.com>
2020-05-16 16:30:33 +0100

    output.zig: split out OutputBackend to be the union / Output is now combination of OutputBackend and Views array

--- src/output.zig
+++ src/output.zig
@@ -6,53 +6,59 @@ const Backend = @import("backend/backend.zig").Backend;
 const BackendType = @import("backend/backend.zig").BackendType;
 const HeadlessOutput = @import("backend/headless.zig").HeadlessOutput;
 const GLFWOutput = @import("backend/glfw.zig").GLFWOutput;
+const View = @import("view.zig").View;
 
 pub var OUTPUTS: Stalloc(void, Output, 16) = undefined;
 pub const OUTPUT_BASE: usize = 1000;
 
-pub const Output = union(BackendType) {
+pub const OutputBackend = union(BackendType) {
     Headless: HeadlessOutput,
     GLFW: GLFWOutput,
+};
+
+pub const Output = struct {
+    backend: OutputBackend,
+    views: [4]View,
 
     const Self = @This();
 
     pub fn begin(self: Self) void {
-        return switch (self) {
+        return switch (self.backend) {
             BackendType.Headless => |headless_output| headless_output.begin(),
             BackendType.GLFW => |glfw_output| glfw_output.begin(),
         };
     }
 
     pub fn end(self: Self) void {
-        return switch (self) {
+        return switch (self.backend) {
             BackendType.Headless => |headless_output| headless_output.end(),
             BackendType.GLFW => |glfw_output| glfw_output.end(),
         };
     }
 
     pub fn swap(self: Self) void {
-        return switch (self) {
+        return switch (self.backend) {
             BackendType.Headless => |headless_output| headless_output.swap(),
             BackendType.GLFW => |glfw_output| glfw_output.swap(),
         };
     }
 
     pub fn shouldClose(self: Self) bool {
-        return switch (self) {
+        return switch (self.backend) {
             BackendType.Headless => |headless_output| headless_output.shouldClose(),
             BackendType.GLFW => |glfw_output| glfw_output.shouldClose(),
         };
     }
 
     pub fn getWidth(self: Self) i32 {
-        return switch (self) {
+        return switch (self.backend) {
             BackendType.Headless => |headless_output| headless_output.getWidth(),
             BackendType.GLFW => |glfw_output| glfw_output.getWidth(),
         };
     }
 
     pub fn getHeight(self: Self) i32 {
-        return switch (self) {
+        return switch (self.backend) {
             BackendType.Headless => |headless_output| headless_output.getHeight(),
             BackendType.GLFW => |glfw_output| glfw_output.getHeight(),
         };
@@ -84,7 +90,7 @@ pub const Output = union(BackendType) {
             }
         }
 
-        return switch (self.*) {
+        return switch (self.backend) {
             BackendType.Headless => |*headless_output| headless_output.deinit(),
             BackendType.GLFW => |*glfw_output| glfw_output.deinit(),
             else => return,
