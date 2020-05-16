5ba754eac1ede1bdbb3359f39bb1aa4f6394fbff
Malcolm Still <malcolm.still@gmail.com>
2020-05-16 22:41:36 +0100

    window.zig: mapped, view, top_link, render, Link
    - Adds Link struct which represents the next / prev in a doubly linked list
    - Window has mapped field to specify whether it's been added to a views window list or not
    - Window has view field to specify its view
    - top_link field contains next / prev toplevel window
    - Adds current() function that returns current BufferedState
    - Adds render() function that does rendering
    - Adds ToplevelIterator for iterating across toplevel windows and toplevelIterator() function to initialise one for the given Window

--- src/window.zig
+++ src/window.zig
@@ -4,6 +4,7 @@ const Client = @import("client.zig").Client;
 const Rectangle = @import("rectangle.zig").Rectangle;
 const LinearFifo = std.fifo.LinearFifo;
 const LinearFifoBufferType = std.fifo.LinearFifoBufferType;
+const View = @import("view.zig").View;
 
 const MAX_WINDOWS = 512;
 pub var WINDOWS: [MAX_WINDOWS]Window = undefined;
@@ -12,6 +13,9 @@ pub const Window = struct {
     index: usize = 0,
     in_use: bool = false,
     client: *Client,
+
+    mapped: bool = false,
+    view: ?*View,
 
     parent: ?*Window,
 
@@ -25,12 +29,12 @@ pub const Window = struct {
     xdg_toplevel_id: ?u32,
     wl_subsurface_id: ?u32,
 
-    input_region_id: ?u32,
-    opaque_region_id: ?u32,
     window_geometry: ?Rectangle,
 
     state: [2]BufferedState = undefined,
     stateIndex: u1 = 0,
+
+    top_link: Link,
 
     title: [128]u8 = undefined,
     app_id: [256]u8 = undefined,
@@ -41,10 +45,53 @@ pub const Window = struct {
     // flip double-buffered state
     pub fn flip(self: *Self) void {
         self.stateIndex +%= 1;
+        self.pending().* = self.current().*;
+    }
+
+    pub fn current(self: *Self) *BufferedState {
+        return &self.state[self.stateIndex];
     }
 
     pub fn pending(self: *Self) *BufferedState {
         return &self.state[self.stateIndex +% 1];
+    }
+
+    pub fn render(self: *Self) !void {
+        if (self.texture) |texture| {
+            renderer.setGeometry(self);
+            try renderer.renderSurface(renderer.PROGRAM, texture);
+        }
+    }
+
+    // ToplevelIterator provides an iterator for moving
+    // between toplevel windows (if the window happens to
+    // be toplevel)
+    pub const ToplevelIterator = struct {
+        current: ?*Window,
+
+        pub fn next(self: *ToplevelIterator) ?*Window {
+            if (self.current) |window| {
+                self.current = window.top_link.next;
+                return window;
+            }
+
+            return null;
+        }
+
+        pub fn prev(self: *ToplevelIterator) ?*Window {
+            if (self.current) |window| {
+                self.current = window.top_link.prev;
+                return window;
+            }
+
+            return null;
+        }
+    };
+
+    pub fn toplevelIterator(self: *Self) ToplevelIterator {
+        return ToplevelIterator {
+            .current = self,
+        };
     }
 
     pub fn deinit(self: *Self) !void {
@@ -58,14 +105,17 @@ pub const Window = struct {
         self.xdg_toplevel_id = null;
         self.wl_subsurface_id = null;
 
-        self.input_region_id = null;
-        self.opaque_region_id = null;
-
         self.state[0].deinit();
         self.state[1].deinit();
 
-        self.width = 0;
-        self.height = 0;
+        if (self.view) |v| {
+            if (v.top == self) {
+                v.top = self.top_link.prev;
+            }
+        }
+        self.view = null;
+        self.top_link.deinit();
+        self.mapped = false;
 
         if (self.texture) |texture| {
             self.texture = null;
@@ -90,8 +140,6 @@ pub fn newWindow(client: *Client, wl_surface_id: u32) !*Window {
             window.wl_buffer_id = null;
             window.xdg_surface_id = null;
             window.xdg_toplevel_id = null;
-            window.input_region_id = null;
-            window.opaque_region_id = null;
 
             window.texture = null;
             window.width = 0;
@@ -110,11 +158,15 @@ pub fn newWindow(client: *Client, wl_surface_id: u32) !*Window {
 const BufferedState = struct {
     sync: bool = false,
 
+    prev: ?*Window,
+    next: ?*Window,
+
     x: i32 = 0,
     y: i32 = 0,
-    width: u32 = 0,
-    height: u32 = 0,
     scale: i32 = 1,
+
+    input_region_id: ?u32,
+    opaque_region_id: ?u32,
 
     min_width: ?i32,
     min_height: ?i32,
@@ -126,11 +178,15 @@ const BufferedState = struct {
     fn deinit(self: *Self) void {
         self.sync = false;
 
+        self.prev = null;
+        self.next = null;
+
         self.x = 0;
         self.y = 0;
-        self.width = 0;
-        self.height = 0;
         self.scale = 1;
+
+        self.input_region_id = null;
+        self.opaque_region_id = null;
 
         self.min_width = null;
         self.min_height = null;
@@ -149,3 +205,21 @@ pub fn releaseWindows(client: *Client) !void {
         i = i + 1;
     }
 }
+
+pub const Link = struct {
+    prev: ?*Window,
+    next: ?*Window,
+
+    pub fn deinit(self: *Link) void {
+        if (self.next) |n| {
+            n.top_link.prev = self.prev;
+        }
+
+        if(self.prev) |p| {
+            p.top_link.next = self.next;
+        }
+
+        self.prev = null;
+        self.next = null;
+    }
+};
