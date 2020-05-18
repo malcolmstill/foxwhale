07df1f4c78c9f8ebfe0a7fd2ef84830a0e539b7b
Malcolm Still <malcolm.still@gmail.com>
2020-05-18 20:38:44 +0100

    wl_seat.zig: set wl_pointer_id and wl_keyboard_id

--- src/wl_seat.zig
+++ src/wl_seat.zig
@@ -5,11 +5,13 @@ const Context = @import("client.zig").Context;
 const Object = @import("client.zig").Object;
 
 fn get_pointer(context: *Context, wl_seat: Object, new_id: u32) anyerror!void {
+    context.client.wl_pointer_id = new_id;
     var wl_pointer = prot.new_wl_pointer(new_id, context, 0);
     try context.register(wl_pointer);
 }
 
 fn get_keyboard(context: *Context, wl_seat: Object, new_id: u32) anyerror!void {
+    context.client.wl_keyboard_id = new_id;
     var wl_keyboard = prot.new_wl_keyboard(new_id, context, 0);
     try context.register(wl_keyboard);
 }
