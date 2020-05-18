1a7debe96b676e0dfe65c04c205faf03bef0e215
Malcolm Still <malcolm.still@gmail.com>
2020-05-18 21:55:03 +0100

    implementations.zig: wl_pointer_impl

--- src/implementations.zig
+++ src/implementations.zig
@@ -6,6 +6,7 @@ const Object = @import("client.zig").Object;
 const fw_control_impl = @import("fw_control.zig");
 const wl_compositor_impl = @import("wl_compositor.zig");
 const wl_display_impl = @import("wl_display.zig");
+const wl_pointer_impl = @import("wl_pointer.zig");
 const wl_registry_impl = @import("wl_registry.zig");
 const wl_region_impl = @import("wl_region.zig");
 const wl_seat_impl = @import("wl_seat.zig");
@@ -23,6 +24,7 @@ pub fn init() void {
 
     wl_compositor_impl.init();
     wl_display_impl.init();
+    wl_pointer_impl.init();
     wl_region_impl.init();
     wl_registry_impl.init();
     wl_seat_impl.init();
