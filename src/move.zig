d40c775235edd30546de9459d32229be604cb0ea
Malcolm Still <malcolm.still@gmail.com>
2020-05-20 02:46:18 +0100

    move.zig: initial commit
    - Take off every zig

---
+++ src/move.zig
@@ -0,0 +1,12 @@
+// Move Zig! Move Zig! Move Zig! Move Zig! You know what you doing?
+// Take off every Zig!
+
+const Window = @import("window.zig").Window;
+
+pub const Move = struct {
+    window: *Window,
+    window_x: i32, // saved Window x
+    window_y: i32, // saved Window y
+    pointer_x: f64, // saved pointer x
+    pointer_y: f64, // saved pointer y
+};
