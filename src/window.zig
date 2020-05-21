147186357e8437ec63cd2edc8fc9bf175f015df0
Malcolm Still <malcolm.still@gmail.com>
2020-05-21 02:29:23 +0100

    window.zig: add a debug function that prints the next and prev indices
    - I used this in debugging an issue where the problem was I was pushing
      the same window onto the view stack more than once (because I was
      pushing every commit rather than checking that the window was
      actually detached)

--- src/window.zig
+++ src/window.zig
@@ -64,6 +64,21 @@ pub const Window = struct {
         return &self.state[self.stateIndex +% 1];
     }
 
+    pub fn debug(self: *Self) void {
+        var next: ?usize = null;
+        var prev: ?usize = null;
+
+        if (self.toplevel.next) |toplevel_next| {
+            next = toplevel_next.index;
+        }
+
+        if (self.toplevel.prev) |toplevel_prev| {
+            prev = toplevel_prev.index;
+        }
+
+        std.debug.warn("debug: {} <-- window[{}] --> {}\n", .{prev, self.index, next});
+    }
+
     pub fn render(self: *Self) anyerror!void {
         var it = self.forwardIterator();
         while(it.next()) |window| {
