6a5330fcaf90bcabaa4376e5f59abc0cbfd76df3
Malcolm Still <malcolm.still@gmail.com>
2020-05-24 03:16:46 +0100

    xdg_surface.zig: implement get_popup
    - consistency: move destroy to top (to match order in .xml)

--- src/implementations/xdg_surface.zig
+++ src/implementations/xdg_surface.zig
@@ -2,9 +2,18 @@ const std = @import("std");
 const prot = @import("../protocols.zig");
 const Context = @import("../client.zig").Context;
 const Object = @import("../client.zig").Object;
+const Positioner = @import("../positioner.zig").Positioner;
 const Rectangle = @import("../rectangle.zig").Rectangle;
 const Window = @import("../window.zig").Window;
 const XdgConfigurations = @import("../window.zig").XdgConfigurations;
+
+fn destroy(context: *Context, xdg_surface: Object) anyerror!void {
+    var window = @intToPtr(*Window, xdg_surface.container);
+    window.xdg_surface_id = null;
+
+    try prot.wl_display_send_delete_id(context.client.wl_display, xdg_surface.id);
+    try context.unregister(xdg_surface);
+}
 
 fn get_toplevel(context: *Context, xdg_surface: Object, new_id: u32) anyerror!void {
     std.debug.warn("get_toplevel: {}\n", .{new_id});
@@ -20,6 +29,31 @@ fn get_toplevel(context: *Context, xdg_surface: Object, new_id: u32) anyerror!vo
     try prot.xdg_surface_send_configure(xdg_surface, serial);
 
     try context.register(xdg_toplevel);
+}
+
+fn get_popup(context: *Context, xdg_surface: Object, new_id: u32, parent_wl_surface: ?Object, xdg_positioner: Object) anyerror!void {
+    var window = @intToPtr(*Window, xdg_surface.container);
+    var positioner = @intToPtr(*Positioner, xdg_positioner.container);
+
+    if (parent_wl_surface) |parent| {
+        window.parent = @intToPtr(*Window, parent.container);
+    }
+    window.positioner = positioner;
+    window.xdg_popup_id = new_id;
+
+    var xdg_popup = prot.new_xdg_popup(new_id, context, @ptrToInt(window));
+
+    var serial = window.client.nextSerial();
+    try prot.xdg_popup_send_configure(
+        xdg_popup,
+        positioner.anchor_rect.x,
+        positioner.anchor_rect.y,
+        positioner.width,
+        positioner.height
+    );
+    try prot.xdg_surface_send_configure(xdg_surface, serial);
+
+    try context.register(xdg_popup);
 }
 
 fn set_window_geometry(context: *Context, xdg_surface: Object, x: i32, y: i32, width: i32, height: i32) anyerror!void {
@@ -68,17 +102,14 @@ fn ack_configure(context: *Context, xdg_surface: Object, serial: u32) anyerror!v
     window.xdg_configurations = XdgConfigurations.init();
 }
 
-fn destroy(context: *Context, xdg_surface: Object) anyerror!void {
-    var window = @intToPtr(*Window, xdg_surface.container);
-    window.xdg_surface_id = null;
 
-    try prot.wl_display_send_delete_id(context.client.wl_display, xdg_surface.id);
-    try context.unregister(xdg_surface);
-}
 
 pub fn init() void {
-    prot.XDG_SURFACE.get_toplevel = get_toplevel;
-    prot.XDG_SURFACE.set_window_geometry = set_window_geometry;
-    prot.XDG_SURFACE.ack_configure = ack_configure;
-    prot.XDG_SURFACE.destroy = destroy;
+    prot.XDG_SURFACE = prot.xdg_surface_interface{
+        .destroy = destroy,
+        .get_toplevel = get_toplevel,
+        .get_popup = get_popup,
+        .set_window_geometry = set_window_geometry,
+        .ack_configure = ack_configure,
+    };
 }
