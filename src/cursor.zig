aae2f737c25ba2d6b58ce2b46a782f32a369c0f9
Malcolm Still <malcolm.still@gmail.com>
2020-05-28 20:14:49 +0100

    cursor.zig: initial commit

---
+++ src/cursor.zig
@@ -0,0 +1,31 @@
+
+var cursor = @embedFile("../assets/cursor.data");
+const renderer = @import("renderer.zig");
+
+pub const Cursor = struct {
+    texture: ?u32,
+    width: i32,
+    height: i32,
+
+    pub fn init() !Cursor {
+        var texture = try renderer.makeTexture(32, 32, 32 * 4, 0, cursor[0..]);
+
+        return Cursor {
+            .width = 32,
+            .height = 32,
+            .texture = texture,
+        };
+    }
+
+    pub fn render(self: *Cursor, x: i32, y: i32) !void {
+        if (self.texture) |texture| {
+            try renderer.scale(1.0, 1.0);
+            try renderer.translate(@intToFloat(f32, x), @intToFloat(f32, y));
+            try renderer.setUniformMatrix(renderer.PROGRAM, "origin", renderer.identity);
+            try renderer.setUniformMatrix(renderer.PROGRAM, "originInverse", renderer.identity);
+            try renderer.setUniformFloat(renderer.PROGRAM, "opacity", 1.0);
+            renderer.setGeometry(self.width, self.height);
+            try renderer.renderSurface(renderer.PROGRAM, texture);
+        }        
+    }
+};
