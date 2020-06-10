e6c14a0d8715bca46e1d2cace8bb3b0b933ab061
Malcolm Still <malcolm.still@gmail.com>
2020-06-10 20:47:51 +0100

    wl_data_device_manager.zig: create objects
    - We still have a crash because we're not implementing any of the functions

--- src/implementations/wl_data_device_manager.zig
+++ src/implementations/wl_data_device_manager.zig
@@ -5,11 +5,13 @@ const Context = @import("../client.zig").Context;
 const Object = @import("../client.zig").Object;
 
 fn create_data_source(context: *Context, wl_data_device_manager: Object, new_id: u32) anyerror!void {
-
+    var wl_data_source = prot.new_wl_data_source(new_id, context, 0);
+    try context.register(wl_data_source);
 }
 
-fn get_data_device(context: *Context, wl_data_device_manager: Object, id: u32, seat: Object) anyerror!void {
-
+fn get_data_device(context: *Context, wl_data_device_manager: Object, new_id: u32, seat: Object) anyerror!void {
+    var wl_data_device = prot.new_wl_data_device(new_id, context, 0);
+    try context.register(wl_data_device);
 }
 
 pub fn init() void {    
