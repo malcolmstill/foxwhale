1e2c8c9b82da2d769c2d4549f9825cf303aa3a3a
Malcolm Still <malcolm.still@gmail.com>
2020-09-05 04:13:54 +0100

    wl_buffer.zig: this was wl_shm_buffer.zig but there is no such wl_shm_buffer protocol
    - The wl_buffer destroy function was specific to shm buffers
    - Make it general for Buffer

---
+++ src/implementations/wl_buffer.zig
@@ -0,0 +1,20 @@
+const std = @import("std");
+const prot = @import("../protocols.zig");
+const shm_buffer = @import("../shm_buffer.zig");
+const Object = @import("../client.zig").Object;
+const Context = @import("../client.zig").Context;
+const Buffer = @import("../buffer.zig").Buffer;
+
+fn destroy(context: *Context, wl_buffer: Object) anyerror!void {
+    var buffer = @intToPtr(*Buffer, wl_buffer.container);
+    // buffer.shm_pool.decrementRefCount(); call this in shm buffer deninit?
+    try buffer.deinit();
+
+    // We still want to do this
+    try prot.wl_display_send_delete_id(context.client.wl_display, wl_buffer.id);
+    try context.unregister(wl_buffer);
+}
+
+pub fn init() void {
+    prot.WL_BUFFER.destroy = destroy;
+}
