3d5685e854b6b6b90ea7a05058a1445feaf1e401
Malcolm Still <malcolm.still@gmail.com>
2020-10-03 22:04:01 +0100

    implementations/wl_display.zig: const

--- src/implementations/wl_display.zig
+++ src/implementations/wl_display.zig
@@ -7,7 +7,7 @@ const Object = @import("../client.zig").Object;
 fn sync(context: *Context, wl_display: Object, new_id: u32) anyerror!void {
     // std.debug.warn("sync with id {}\n", .{new_id});
 
-    var wl_callback = prot.new_wl_callback(new_id, context, 0);
+    const wl_callback = prot.new_wl_callback(new_id, context, 0);
     try prot.wl_callback_send_done(wl_callback, context.client.nextSerial());
     try prot.wl_display_send_delete_id(wl_display, new_id);
 }
@@ -15,7 +15,7 @@ fn sync(context: *Context, wl_display: Object, new_id: u32) anyerror!void {
 fn get_registry(context: *Context, wl_display: Object, new_id: u32) anyerror!void {
     std.debug.warn("get_registry with id {}\n", .{new_id});
 
-    var wl_registry = prot.new_wl_registry(new_id, context, 0);
+    const wl_registry = prot.new_wl_registry(new_id, context, 0);
     context.client.wl_registry_id = new_id;
 
     try prot.wl_registry_send_global(wl_registry, 1, "wl_compositor\x00", 4);
@@ -23,7 +23,7 @@ fn get_registry(context: *Context, wl_display: Object, new_id: u32) anyerror!voi
     try prot.wl_registry_send_global(wl_registry, 3, "wl_seat\x00", 4);
     try prot.wl_registry_send_global(wl_registry, 4, "xdg_wm_base\x00", 1);
 
-    var output_base: u32 = out.OUTPUT_BASE;
+    const output_base: u32 = out.OUTPUT_BASE;
     var it = out.OUTPUTS.iterator();
     while(it.next()) |output| {
         try prot.wl_registry_send_global(wl_registry, output_base, "wl_output\x00", 2);
