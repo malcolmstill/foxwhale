9b27bd47626e862346c1aac62afc200ccfe10815
Malcolm Still <malcolm.still@gmail.com>
2024-02-28 03:51:50 +0000

    Clean up wire

--- src/wl/wire.zig
+++ src/wl/wire.zig
@@ -36,32 +36,32 @@ pub fn Wire(comptime WlMessage: type) type {
             length: u16,
         };
 
-        pub fn startRead(self: *Self) !void {
-            const n = try txrx.recvMsg(self.fd, self.rx_buf[self.rx_write_offset..], &self.rx_fds);
+        pub fn startRead(wire: *Self) !void {
+            const n = try txrx.recvMsg(wire.fd, wire.rx_buf[wire.rx_write_offset..], &wire.rx_fds);
 
-            self.rx = io.fixedBufferStream(self.rx_buf[0 .. self.rx_write_offset + n]);
+            wire.rx = io.fixedBufferStream(wire.rx_buf[0 .. wire.rx_write_offset + n]);
         }
 
-        pub fn finishRead(self: *Self) !void {
-            const read_offset = try self.rx.getPos();
-            const buffer_end = try self.rx.getEndPos();
+        pub fn finishRead(wire: *Self) !void {
+            const read_offset = try wire.rx.getPos();
+            const buffer_end = try wire.rx.getEndPos();
 
             const n = buffer_end - read_offset;
 
-            self.rx_write_offset = n;
+            wire.rx_write_offset = n;
 
-            mem.copyForwards(u8, self.rx_buf[0..n], self.rx_buf[read_offset .. read_offset + n]);
+            mem.copyForwards(u8, wire.rx_buf[0..n], wire.rx_buf[read_offset .. read_offset + n]);
         }
 
         // Is objects just client?
-        pub fn readEvent(self: *Self, comptime C: type, objects: anytype, comptime field: []const u8) !?WlMessage {
-            const rdr = self.rx.reader();
+        pub fn readEvent(wire: *Self, comptime C: type, objects: anytype, comptime field: []const u8) !?WlMessage {
+            const rdr = wire.rx.reader();
 
             // We need to have read at least a header
-            const remaining = (try self.rx.getEndPos()) - (try self.rx.getPos());
+            const remaining = (try wire.rx.getEndPos()) - (try wire.rx.getPos());
             if (remaining < @sizeOf(u32) + @sizeOf(u16) + @sizeOf(u16)) return null;
 
-            const start = try self.rx.getPos();
+            const start = try wire.rx.getPos();
 
             const header = Header{
                 .id = try rdr.readInt(u32, endian),
@@ -78,18 +78,18 @@ pub fn Wire(comptime WlMessage: type) type {
 
             const event = try object.readMessage(C, objects, field, header.opcode);
 
-            const actual_length = (try self.rx.getPos()) - start;
+            const actual_length = (try wire.rx.getPos()) - start;
             if (actual_length != header.length) return error.MessageWrongLength;
 
             return event;
         }
 
-        pub fn nextU32(self: *Self) !u32 {
-            return self.rx.reader().readInt(u32, endian);
+        pub fn nextU32(wire: *Self) !u32 {
+            return wire.rx.reader().readInt(u32, endian);
         }
 
-        pub fn nextI32(self: *Self) !i32 {
-            return self.rx.reader().readInt(i32, endian);
+        pub fn nextI32(wire: *Self) !i32 {
+            return wire.rx.reader().readInt(i32, endian);
         }
 
         // we just expose a pointer to the rx_buf for the
@@ -97,16 +97,16 @@ pub fn Wire(comptime WlMessage: type) type {
         // after the dispatch function returns and so we cannot
         // hang on to this pointer. If we want to retain the string
         // we should copy it.
-        pub fn nextString(self: *Self) ![]u8 {
-            const length = try self.nextU32();
+        pub fn nextString(wire: *Self) ![]u8 {
+            const length = try wire.nextU32();
             const padded_length = length + padding(length);
 
-            const start = try self.rx.getPos();
+            const start = try wire.rx.getPos();
 
-            try self.rx.reader().skipBytes(padded_length, .{});
+            try wire.rx.reader().skipBytes(padded_length, .{});
 
             var s: []u8 = undefined;
-            s.ptr = @ptrCast(&self.rx_buf[start]);
+            s.ptr = @ptrCast(&wire.rx_buf[start]);
             s.len = length;
 
             return s;
@@ -117,84 +117,84 @@ pub fn Wire(comptime WlMessage: type) type {
         // after the dispatch function returns and so we cannot
         // hang on to this pointer. If we want to retain the array
         // we should copy it.
-        pub fn nextArray(self: *Self) ![]u8 {
-            const length = try self.nextU32();
+        pub fn nextArray(wire: *Self) ![]u8 {
+            const length = try wire.nextU32();
             const padded_length = length + padding(length);
 
-            const start = try self.rx.getPos();
+            const start = try wire.rx.getPos();
 
-            try self.rx.reader().skipBytes(padded_length, .{});
+            try wire.rx.reader().skipBytes(padded_length, .{});
 
             var s: []u8 = undefined;
-            s.ptr = &self.rx_buf[start];
+            s.ptr = &wire.rx_buf[start];
             s.len = length;
             return s;
         }
 
-        pub fn nextFd(self: *Self) !i32 {
-            return self.rx_fds.readItem() orelse return error.FdReadFailed;
+        pub fn nextFd(wire: *Self) !i32 {
+            return wire.rx_fds.readItem() orelse return error.FdReadFailed;
         }
 
-        pub fn startWrite(self: *Self) !void {
-            self.tx = io.fixedBufferStream(self.tx_buf[0..]);
-            try self.tx.writer().writeInt(u32, 0, endian);
-            try self.tx.writer().writeInt(u16, 0, endian);
-            try self.tx.writer().writeInt(u16, 0, endian);
+        pub fn startWrite(wire: *Self) !void {
+            wire.tx = io.fixedBufferStream(wire.tx_buf[0..]);
+            try wire.tx.writer().writeInt(u32, 0, endian);
+            try wire.tx.writer().writeInt(u16, 0, endian);
+            try wire.tx.writer().writeInt(u16, 0, endian);
         }
 
-        pub fn finishWrite(self: *Self, id: u32, opcode: u16) !void {
-            const end_pos = math.cast(u16, try self.tx.getPos()) orelse return error.EndPositionMustBeU16;
-            self.tx.reset();
+        pub fn finishWrite(wire: *Self, id: u32, opcode: u16) !void {
+            const end_pos = math.cast(u16, try wire.tx.getPos()) orelse return error.EndPositionMustBeU16;
+            wire.tx.reset();
 
-            try self.tx.writer().writeInt(u32, id, endian);
-            try self.tx.writer().writeInt(u16, opcode, endian);
-            try self.tx.writer().writeInt(u16, end_pos, endian);
+            try wire.tx.writer().writeInt(u32, id, endian);
+            try wire.tx.writer().writeInt(u16, opcode, endian);
+            try wire.tx.writer().writeInt(u16, end_pos, endian);
 
-            _ = txrx.sendMsg(self.fd, self.tx_buf[0..end_pos], &self.tx_fds) catch |err| switch (err) {
+            _ = txrx.sendMsg(wire.fd, wire.tx_buf[0..end_pos], &wire.tx_fds) catch |err| switch (err) {
                 error.BrokenPipe => return,
                 else => return err,
             };
         }
 
-        pub fn putU32(self: *Self, value: u32) !void {
-            try self.tx.writer().writeInt(u32, value, endian);
+        pub fn putU32(wire: *Self, value: u32) !void {
+            try wire.tx.writer().writeInt(u32, value, endian);
         }
 
-        pub fn putI32(self: *Self, value: i32) !void {
-            try self.tx.writer().writeInt(i32, value, endian);
+        pub fn putI32(wire: *Self, value: i32) !void {
+            try wire.tx.writer().writeInt(i32, value, endian);
         }
 
-        pub fn putFd(self: *Self, value: i32) !void {
-            try self.tx_fds.writeItem(value);
+        pub fn putFd(wire: *Self, value: i32) !void {
+            try wire.tx_fds.writeItem(value);
         }
 
-        pub fn putFixed(self: *Self, value: f64) !void {
-            try self.putI32(doubleToFixed(value));
+        pub fn putFixed(wire: *Self, value: f64) !void {
+            try wire.putI32(doubleToFixed(value));
         }
 
-        pub fn putArray(self: *Self, array: []u8) !void {
+        pub fn putArray(wire: *Self, array: []u8) !void {
             const length = math.cast(u32, array.len) orelse return error.ArrayTooBig;
             const padded_length = length + padding(length);
 
-            try self.putU32(length);
+            try wire.putU32(length);
 
-            const start = try self.tx.getPos();
-            try self.tx.seekBy(padded_length);
+            const start = try wire.tx.getPos();
+            try wire.tx.seekBy(padded_length);
 
-            mem.copyForwards(u8, self.tx_buf[start .. start + length], array);
+            mem.copyForwards(u8, wire.tx_buf[start .. start + length], array);
         }
 
         // string is assumed to have a null byte within its contents / length
-        pub fn putString(self: *Self, string: []const u8) !void {
+        pub fn putString(wire: *Self, string: []const u8) !void {
             const length = math.cast(u32, string.len) orelse return error.ArrayTooBig;
             const padded_length = length + padding(length);
 
-            try self.putU32(length);
-            const start = try self.tx.getPos();
+            try wire.putU32(length);
+            const start = try wire.tx.getPos();
 
-            try self.tx.seekBy(padded_length);
+            try wire.tx.seekBy(padded_length);
 
-            std.mem.copyForwards(u8, self.tx_buf[start .. start + length], string);
+            std.mem.copyForwards(u8, wire.tx_buf[start .. start + length], string);
         }
     };
 }
